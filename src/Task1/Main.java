629180031f383ec89b132d9ae459fcc5ff369c82
ярослав <yaracrut245@gmail.com>
2024-08-07 22:45:32 +0500

    1

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -1,2 +1,34 @@
-package Task1;public class Main {
+package Task1;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = new LinkedList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(7);
+        numbers.add(8);
+
+        Sum summOf = (n) -> n % 2 == 0;
+        System.out.println(sumItnegerList(numbers, summOf));
+    }
+
+    public static int sumItnegerList (List<Integer> listExamp, Sum sum){
+        int result = 0;
+        for (int i: listExamp) {
+            if (sum.isEven(i)){
+                result+=i;
+            }
+        }
+        return result;
+    }
+}
+
+interface Sum {
+    boolean isEven (int a);
 }
--- src/Task2/Main.java
+++ src/Task2/Main.java
@@ -1,5 +1,19 @@
 package Task2;
 
-public class Main {
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
 
+public class Main {
+    public static void main(String[] args) {
+        List<String> stroke = new LinkedList<>();
+        stroke.add("aaa");
+        stroke.add("bb");
+        stroke.add("ccccc");
+        stroke.add("dddd");
+
+        String max = Collections.max(stroke, (o1, o2) -> o1.length()-o2.length());
+        System.out.println(max);
+    }
 }
